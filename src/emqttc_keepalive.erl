722773d00b99b69a109e1068e2629af439e11d53
liucy2007975 <455584546@qq.com>
2016-05-05 18:07:53 +0800

    log keepalive msg.

--- src/emqttc_keepalive.erl
+++ src/emqttc_keepalive.erl
@@ -65,6 +65,7 @@ start(KeepAlive = #keepalive{socket = Socket, stat_name = StatName,
     case emqttc_socket:getstat(Socket, [StatName]) of
         {ok, [{StatName, StatVal}]} ->
             %% io:format("client ~p send keepalived status: ~p~n", [Socket,StatName]),
+            io:format("TimeoutSec ~p   ~p~n",[TimeoutSec,TimeoutMsg]),
             Ref = erlang:send_after(TimeoutSec*1000, self(), TimeoutMsg),
             {ok, KeepAlive#keepalive{stat_val = StatVal, timer_ref = Ref}};
         {error, Error} ->
